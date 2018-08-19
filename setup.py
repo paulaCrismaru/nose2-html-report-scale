04997ea7bd85fcd6a25127e7ee9ba214bc9c699d
Paula Crismaru <paulacrismaru@gmail.com>
2018-08-19 10:53:53 +0300

    Add templates directory and requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setuptools.setup(
     name="html_report_scale",
     packages=["html_report_scale"],
     version="0.0.1",
+    package_data={
+        'templates': [
+            'report.html'
+        ],
+    },
+    install_requires=[
+        'jinja2', 'nose2'
+    ]
+
 )
